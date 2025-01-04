0c585ff1e3341221acb6f748a34b0056e1ccb218
Callum Coy <callumcoy@ymail.com>
2025-01-04 17:52:10 -0500

    Initiial upload

---
+++ Checkers.py
@@ -0,0 +1,67 @@
+from pprint import pprint
+
+def main():
+
+    boardLayout = initCheckerBoard(9,9)
+    playerBoard = initGame(2,2, boardLayout)
+
+    pprint(boardLayout)
+    pprint(playerBoard)
+
+# Makes a basic checker board using the specified diemensions
+def initCheckerBoard(xMax,yMax):
+    board = []
+
+    for y in range(yMax):
+        curRow=[]
+        for x in range(xMax):
+
+            # Alternates between square colors, using the the row # to offset every other row.
+            if (x+y) % 2:
+                curRow.append("b")
+            else:
+                curRow.append("w")
+            continue
+
+        # Inputs the new row into the board
+        board.append(curRow)
+
+    return(board)
+
+# Populates the board chips based upon the requested rows
+def initGame(chadRows, alphaRows, board):
+
+    # Errors out on if invalid inputs 
+    if chadRows+alphaRows >= len(board) or chadRows < 1 or alphaRows < 1:
+        return(SystemError)
+
+    playBoard=[]
+    count = 0
+
+    for row in board:
+        
+        playrow = []
+
+        if count < chadRows:
+            inputCar = "c"
+        elif count < len(board) - alphaRows:
+            inputCar = ""
+        else:
+            inputCar = "a"
+
+        for square in row:
+            if square == "w":
+                playrow.append(inputCar)
+            else:
+                playrow.append("")
+            continue
+
+        playBoard.append(playrow)
+        count = count + 1
+        continue
+
+    return(playBoard)
+
+
+if __name__ =="__main__":
+    main()
